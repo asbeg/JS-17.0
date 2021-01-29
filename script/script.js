1d2f43d5290144af82256d8a71ffdabb70365b31
BegaiAs <begaiasanbekova@gmail.com>
2021-01-29 16:13:01 +0100

    Lesson02

--- script/script.js
+++ script/script.js
@@ -1,9 +1,29 @@
 let money,
+    budgetDay,
     income,
     addExpenses,
     deposit,
     mission,
     period;
 
-alert('Hello World!');
-console.log('Test!');
+money = 1200;
+income = "Freelance";
+addExpenses = "Taxi, School";
+deposit = false;
+mission = 80000;
+period = 6;
+budgetDay = money/30;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log("Период равен: " + period + " месяцев");
+console.log("Цель заработать: " + mission + " рублей");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log("дневной бюджет: " + budgetDay);
+
+
+
