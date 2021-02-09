0bf97e1e282e71649fbad33a4849f86c81f3d8ee
BegaiAs <begaiasanbekova@gmail.com>
2021-02-09 21:42:22 +0100

    fix variables

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-'use strict'
 /*let money = 0;
 
 const isNumber = function (n) {
@@ -155,51 +154,56 @@ for (let key in appData) {
 *!/*/
 
 const startBtn = document.getElementById('start'),
-    btnPlusIncomeAdd = document.getElementsByTagName('button')[0],
-    btnPlusExpensesAdd = document.getElementsByTagName('button')[1],
+    // + btn
+    incomeAdd = document.getElementsByTagName('button')[0],
+    expensesAdd = document.getElementsByTagName('button')[1],
+    // checkbox
     depositCheck = document.querySelector('#deposit-check'),
+    //Возможный доход
     additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
 
-    resultTotalBudgetDayValue = document.getElementsByClassName('result-total budget_day-value')[0],
-    resultTotalExpensesMonthValue = document.getElementsByClassName('result-total expenses_month-value')[0],
-    resultTotalAdditionalIncomeValue = document.getElementsByClassName('result-total additional_income-value')[0],
-    resultTotalAdditionalExpensesValue = document.getElementsByClassName('result-total additional_expenses-value')[0],
+    // e
+    budgetMonthValue = document.getElementsByClassName('budget_month-value')[0],
+    budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
+    expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0],
+    additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0],
+    additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0],
+    incomePeriodValue = document.getElementsByClassName('income_period-value')[0],
+    targetMonthValue = document.getElementsByClassName('target_month-value')[0],
 
-    resultTotalIncomePeriodValue = document.getElementsByClassName('result-total income_period-value')[0],
-    resultTotalTargetMonthValue = document.getElementsByClassName('result-total target_month-value')[0],
+    // range
+    periodSelect = document.querySelector('input[class=period-select]'),
 
-    targetAmount = document.getElementsByClassName('target-amount')[0],
-    depositAmount = document.querySelector('div.deposit-calc input[class=deposit-amount]'),
-    depositPercent = document.querySelector('div.deposit-calc input[class=deposit-percent]'),
-    additionalExpensesItem = document.querySelector('div.additional_expenses input[class=additional_expenses-item]'),
-    expensesAmount = document.querySelector('div.expenses-items input[class=expenses-amount]'),
-    expenseTitle = document.querySelector('div.expenses input[class=expenses-title]'),
-    periodSelect = document.querySelector('div.period input[class=period-select]'),
-    salaryAmount = document.querySelector('div.salary input[class=salary-amount]'),
-    incomeTitle = document.querySelector('div.income input[class=income-title]'),
-    incomeAmount = document.querySelector('div.income input[class=income-amount]');
+    // f
+    targetAmount = document.querySelector('input[class=target-amount]'),
+    additionalExpensesItem = document.querySelector('input[class=additional_expenses-item]'),
+    expensesAmount = document.querySelector('input[class=expenses-amount]'),
+    expenseTitle = document.querySelector('input[class=expenses-title]'),
+    salaryAmount = document.querySelector('input[class=salary-amount]'),
+    incomeTitle = document.querySelector('input[class=income-title]'),
+    incomeAmount = document.querySelector('input[class=income-amount]');
+
 
 console.log(startBtn);
-console.log(btnPlusIncomeAdd);
-console.log(btnPlusExpensesAdd);
+console.log(incomeAdd);
+console.log(expensesAdd);
 console.log(depositCheck);
 console.log(additionalIncomeItem);
 
-console.log(resultTotalBudgetDayValue);
-console.log(resultTotalExpensesMonthValue);
-console.log(resultTotalAdditionalIncomeValue);
-console.log(resultTotalAdditionalExpensesValue);
+console.log(budgetMonthValue);
+console.log(budgetDayValue);
+console.log(expensesMonthValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
 
-console.log(resultTotalIncomePeriodValue);
-console.log(resultTotalTargetMonthValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
 
+console.log(periodSelect);
 console.log(targetAmount);
-console.log(depositAmount);
-console.log(depositPercent);
 console.log(additionalExpensesItem);
 console.log(expensesAmount);
 console.log(expenseTitle);
-console.log(periodSelect);
 console.log(salaryAmount);
 console.log(incomeTitle);
 console.log(incomeAmount);
