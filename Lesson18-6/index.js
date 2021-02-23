69bc7b3d9fedac7ef79b4a20709ac664b55e0a06
BegaiAs <begaiasanbekova@gmail.com>
2021-02-23 21:50:41 +0100

    fix myDate

--- Lesson18-6/index.js
+++ Lesson18-6/index.js
@@ -1,26 +1,24 @@
 let date = new Date(),
     nextDate = new Date("January 1, 2022"),
-    dateNow = date.toLocaleTimeString('ru'),
     weekDay = date.toLocaleDateString('ru', {weekday: 'long'}),
     hour = date.toLocaleTimeString('en-US'),
     msPerDay = 24 * 60 * 60 * 1000,
-    daysLeft = Math.round((nextDate.getTime()
-        - date.getTime()) / msPerDay);
+    daysLeft = Math.round((nextDate.getTime() - date.getTime()) / msPerDay),
+    hours = date.getHours();
 
 let p1 = document.querySelector('#p1'),
-    p2 = document.querySelector('#p2').textContent = `Сегодня: ${weekDay}`;
-    p3 = document.querySelector('#p3').textContent = `Текущее время: ${hour}`;
+    p2 = document.querySelector('#p2').textContent = `Сегодня: ${weekDay}`,
+    p3 = document.querySelector('#p3').textContent = `Текущее время: ${hour}`,
     p4 = document.querySelector('#p4').textContent = `До нового года осталось ${daysLeft} дней`;
 
-
 function myDate() {
-    if (dateNow > '00' && dateNow < '05') {
+    if (hours >= 0 && hours < 5) {
         p1.textContent = `Доброй ночи!`;
-    } else if (dateNow > '05' && dateNow < '11') {
+    } else if (hours >= 5 && hours < 11) {
         p1.textContent = `Доброе утро!`;
-    } else if (dateNow > '11' && dateNow < '17') {
+    } else if (hours >= 11 && hours < 17) {
         p1.textContent = `Добрый день!`;
-    } else if (dateNow > '17') {
+    } else {
         p1.textContent = `Добрый вечер!`;
     }
 }
