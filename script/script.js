490c70c2023add155145a8d310727381d9e2870a
BegaiAs <begaiasanbekova@gmail.com>
2021-02-24 18:06:57 +0100

    Lesson19 PopUpAnimate

--- script/script.js
+++ script/script.js
@@ -45,5 +45,84 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     countTimer('24 February 2021');
+
+    //меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        }
+
+        btnMenu.addEventListener('click', handlerMenu);
+
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+    };
+    toggleMenu();
+
+
+    //popup okno(оставить заявку)
+    /*    const togglePopup = () =>{
+            const popup = document.querySelector('.popup'),
+                popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose=document.querySelector('.popup-close');
+
+            popupBtn.forEach((elem) =>{
+               elem.addEventListener('click', () =>{
+                 popup.style.display = 'block';
+               });
+
+                popupClose.addEventListener('click', ()=>{
+                    popup.style.display = 'none';
+                });
+            });
+        };togglePopup();*/
+    const animatedTogglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content'),
+            popupData = {
+                count: 200,
+                speed: 3,
+                startPos: 200,
+                endPos: 0
+            };
+
+        const showPopup = () => {
+
+            popupData.startPos > popupData.endPos ?
+                popupData.count -= popupData.speed :
+                popupData.count += popupData.speed;
+            popupContent.style.transform = `translateY(${popupData.count}px)`;
+
+            if (popupData.startPos > popupData.endPos ?
+                popupData.count > popupData.endPos :
+                popupData.count < popupData.endPos) {
+                requestAnimationFrame(showPopup);
+            }
+        };
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (screen.width > 768) {
+                    popupData.count = popupData.startPos;
+                    requestAnimationFrame(showPopup);
+                }
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+
+    animatedTogglePopUp();
 });
 
