a25c5ae753b1a2bbc41365b21e0cd63f5a256cb1
BegaiAs <begaiasanbekova@gmail.com>
2021-02-28 12:37:43 +0100

    Lesson21 Slider

--- script/script.js
+++ script/script.js
@@ -16,8 +16,6 @@ window.addEventListener('DOMContentLoaded', function () {
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 60 / 60);
-            //  day = Math.floor(timeRemaining / 60 / 60 / 24);
-
 
             return {timeRemaining, hours, minutes, seconds};
         }
@@ -48,13 +46,11 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //меню
     const toggleMenu = () => {
-        const handlerMenu = () => {
+        const handlerMenu = (event) => {
             const target = event.target;
-            console.log(target);
             const displayMenu = () => {
                 document.querySelector('menu').classList.toggle('active-menu');
             };
-
             if (target.closest('.menu') ||
                 (!target.closest('menu') &&
                     document.querySelector('menu').classList.contains('active-menu'))) {
@@ -167,6 +163,98 @@ window.addEventListener('DOMContentLoaded', function () {
 
     tabs();
 
+    //слайдер
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+            dot = document.querySelectorAll('.dot'),
+            slider = document.querySelector('.portfolio-content');
+
+        let currentSlide = 0,
+            interval;
+
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+        };
+
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide = currentSlide < slide.length - 1 ? currentSlide + 1 : 0;
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+
+        const startSlide = (time = 2000) => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+
+        slider.addEventListener('click', event => {
+            event.preventDefault();
+
+            const target = event.target;
+
+            if (target.matches('.portfolio-btn, .dot')) {
+                prevSlide(slide, currentSlide, 'portfolio-item-active');
+                prevSlide(dot, currentSlide, 'dot-active');
+
+                if (target.matches('#arrow-right')) {
+                    currentSlide++;
+                } else if (target.matches('#arrow-left')) {
+                    currentSlide--;
+                } else if (target.matches('.dot')) {
+                    dot.forEach((elem, index) => {
+                        if (elem === target) {
+                            currentSlide = index;
+                        }
+                    });
+                }
+
+                if (currentSlide >= slide.length) currentSlide = 0;
+                if (currentSlide < 0) currentSlide = slide.length - 1;
+
+                nextSlide(slide, currentSlide, 'portfolio-item-active');
+                nextSlide(dot, currentSlide, 'dot-active');
+            }
+        });
+
+        slider.addEventListener('mouseover', event => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                stopSlide();
+            }
+        });
+
+        slider.addEventListener('mouseout', event => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                startSlide();
+            }
+        });
+
+        startSlide();
+    };
+
+    // на страницу добавлять точки с классом dot равному количеству слайдов
+    const addDot = () => {
+        const portfolioItem = document.querySelectorAll('.portfolio-item'),
+            portfolioDots = document.querySelector('.portfolio-dots');
+
+        portfolioItem.forEach(() => {
+            const dot = document.createElement('li');
+            dot.classList.add('dot');
+            portfolioDots.appendChild(dot);
+        });
+
+        portfolioDots.children[0].classList.add('dot-active');
+    };
+    addDot();
+    slider();
 
 });
 
